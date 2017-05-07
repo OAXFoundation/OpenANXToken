f4ef8a5e1a2c2d4e6b1bcd1819da7fa4e21667f7
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2017-05-08 02:30:57 +1000

    Update

--- contracts/SuperDEXICO.sol
+++ contracts/SuperDEXICO.sol
@@ -78,8 +78,7 @@ contract ERC20Token {
     // The transferFrom method is used for a withdraw workflow, allowing contracts to send
     // tokens on your behalf, for example to "deposit" to a contract address and/or to charge
     // fees in sub-currencies; the command should fail unless the _from account has
-    // deliberately authorized the sender of the message via some mechanism; we propose
-    // these standardized APIs for approval:
+    // deliberately authorized the sender of the message via the approve(...) function
     function transferFrom(
         address _from,
         address _to,
